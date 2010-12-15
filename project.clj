b91c6a484ef3121fe72dfe4ca57d8223d3cb7126
Chris Keating <chris@pntfin.com>
2010-12-15 08:25:15 -0600

    initial project

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject ttl "0.0.1"
+    :description "A website for table tennis leagues"
+    :url "http://pdex.github.com"
+    :dependencies [
+        [org.clojure/clojure "1.2.0"]
+        [org.clojure/clojure-contrib "1.2.0"]
+        [compojure "0.5.3"]
+        [hiccup "0.3.1"]
+    ]
+)
