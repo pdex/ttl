dae17c91b3b42c7acceb8915b8b6e993ae5e2193
Chris Keating <christopherkeating@gmail.com>
2010-12-26 20:54:01 -0600

    working app

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
 (defproject ttl "0.0.1"
     :description "A website for table tennis leagues"
+    :aot [pdexttl.app]
     :url "http://pdex.github.com"
-    :dependencies [
-        [org.clojure/clojure "1.2.0"]
-        [org.clojure/clojure-contrib "1.2.0"]
-        [compojure "0.5.3"]
-        [ring/ring-jetty-adapter "0.3.1"]
-        [hiccup "0.3.1"]
-    ]
-    :dev-dependencies [
-        [lein-run "1.0.0"]
-    ]
-)
+    :dependencies [[org.clojure/clojure "1.2.0"]
+                   [org.clojure/clojure-contrib "1.2.0"]
+                   [compojure "0.5.3"]
+                   [ring/ring-jetty-adapter "0.3.1"]
+                   [hiccup "0.3.1"]]
+    :dev-dependencies [[lein-run "1.0.0"]]
+    :compile-path "war/WEB-INF/classes"
+    :library-path "war/WEB-INF/lib")
