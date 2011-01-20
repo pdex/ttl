1ee2d6eb716b3cb134e94032810cefd1cb92463b
Chris Keating <christopherkeating@gmail.com>
2011-01-19 19:23:43 -0600

    compiling is working, good time to save

--- project.clj
+++ project.clj
@@ -6,7 +6,12 @@
                    [org.clojure/clojure-contrib "1.2.0"]
                    [compojure "0.5.3"]
                    [ring/ring-jetty-adapter "0.3.1"]
+                   [appengine "0.4.3-SNAPSHOT"]
+                   [com.google.appengine/appengine-api-1.0-sdk "1.4.0"]
+                   [com.google.appengine/appengine-api-labs "1.4.0"]
                    [hiccup "0.3.1"]]
-    :dev-dependencies [[lein-run "1.0.0"]]
+    :dev-dependencies [[lein-run "1.0.0"]
+                   [com.google.appengine/appengine-api-stubs "1.4.0"]
+                   [com.google.appengine/appengine-local-runtime "1.4.0"]]
     :compile-path "war/WEB-INF/classes"
     :library-path "war/WEB-INF/lib")
--- src/pdexttl/app.clj
+++ src/pdexttl/app.clj
@@ -1,11 +1,49 @@
 (ns pdexttl.app
     (:gen-class :extends javax.servlet.http.HttpServlet)
-    (:use compojure.core
-        ring.util.servlet)
+    (:use 
+          compojure.core
+          [ring.util.servlet :only [defservice]]
+          [ring.util.response :only [redirect]]
+          [clojure.contrib.string :only (lower-case)]
+          hiccup.core
+          hiccup.page-helpers
+          hiccup.form-helpers
+          appengine.datastore)
     (:require [compojure.route :as route]))
 
+
+(defn page [elements]
+    (html [:html
+        [:title (:title elements)]
+        [:body (:body elements)]]))
+(defentity Player () (
+    (email :key lower-case :serialize lower-case)
+    (name)
+    (ranking)))
+
+;(def get-players ["player1" "player2" "player3"])
+;(select "player" ))
+(def nothing '["1" "2"])
+;(def list-players-page (page 
+;    {:title "List Players"
+;     :body [:ol (map #([:li %1]) (nothing))]}))
+(def add-player-form (form-to [:post "/addPlayer.post"
+    [:fieldset [:legend "Add a new player"]
+        [:ol [:li [:label {:for :name} "Name"] (text-field :name)]
+             [:li [:label {:for :ranking} "Ranking"] (text-field :ranking)]]
+        [:button {:type "submit"} "Add!"]]]))
+
+(defn add-player [email name ranking]
+    ;(create-entity (player {:email email :name name :ranking ranking}))
+    (redirect "/"))
+
 (defroutes example
-    (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+    (GET "/" [] (page {
+        :title "This is the title"
+        :body "<h1>Hello World Wide Web!</h1>"}))
+    (GET "/addPlayer" [] (page {:title "Add Player" :body (add-player-form)}))
+    (POST "/addPlayer.post" [name ranking] (add-player name ranking))
+    ;(POST "/newSeason" [name] (create-season name))
     (route/not-found "Page not found"))
 
 (defservice example)
