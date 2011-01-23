cc938894858208a59c1093c7983a2396f0928c41
Chris Keating <christopherkeating@gmail.com>
2011-01-23 15:49:21 -0600

    compilation is working again

--- project.clj
+++ project.clj
@@ -2,16 +2,17 @@
     :description "A website for table tennis leagues"
     :aot [pdexttl.app]
     :url "http://pdex.github.com"
-    :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]
-                   [compojure "0.5.3"]
-                   [ring/ring-jetty-adapter "0.3.1"]
-                   [appengine "0.4.3-SNAPSHOT"]
-                   [com.google.appengine/appengine-api-1.0-sdk "1.4.0"]
-                   [com.google.appengine/appengine-api-labs "1.4.0"]
-                   [hiccup "0.3.1"]]
-    :dev-dependencies [[lein-run "1.0.0"]
-                   [com.google.appengine/appengine-api-stubs "1.4.0"]
-                   [com.google.appengine/appengine-local-runtime "1.4.0"]]
+    :dependencies [[compojure "0.4.0-RC3"]
+        [ring/ring-servlet "0.2.1"]
+        [hiccup "0.2.4"]
+        [appengine "0.2"]
+        [com.google.appengine/appengine-api-1.0-sdk "1.4.0"]
+        [com.google.appengine/appengine-api-labs "1.4.0"]]
+    :dev-dependencies [[swank-clojure "1.2.0"]
+        [ring/ring-jetty-adapter "0.2.0"]
+        [com.google.appengine/appengine-local-runtime "1.4.0"]
+        [com.google.appengine/appengine-api-stubs "1.4.0"]]
+    :repl-init-script "src/local-dev.clj"
+    :repositories {"maven-gae-plugin" "http://maven-gae-plugin.googlecode.com/svn/repository"}
     :compile-path "war/WEB-INF/classes"
     :library-path "war/WEB-INF/lib")
--- src/pdexttl/app.clj
+++ src/pdexttl/app.clj
@@ -1,48 +1,48 @@
 (ns pdexttl.app
-    (:gen-class :extends javax.servlet.http.HttpServlet)
-    (:use 
-          compojure.core
-          [ring.util.servlet :only [defservice]]
-          [ring.util.response :only [redirect]]
-          [clojure.contrib.string :only (lower-case)]
-          hiccup.core
-          hiccup.page-helpers
-          hiccup.form-helpers
-          appengine.datastore)
-    (:require [compojure.route :as route]))
+  (:gen-class :extends javax.servlet.http.HttpServlet)
+  (:use compojure.core
+        [ring.util.servlet   :only [defservice]]
+        [ring.util.response  :only [redirect]]
+        [hiccup.core         :only [h html]]
+        [hiccup.page-helpers :only [doctype include-css link-to xhtml-tag]]
+        [hiccup.form-helpers :only [form-to text-area text-field]])
+  (:import (com.google.appengine.api.datastore Query))
+  (:require [compojure.route          :as route]
+            [appengine.datastore.core :as ds]
+            [appengine.users          :as users]))
 
+(set! *warn-on-reflection* true)
 
 (defn page [elements]
     (html [:html
         [:title (:title elements)]
         [:body (:body elements)]]))
-(defentity Player () (
-    (email :key lower-case :serialize lower-case)
-    (name)
-    (ranking)))
 
-;(def get-players ["player1" "player2" "player3"])
-;(select "player" ))
-(def nothing '["1" "2"])
-;(def list-players-page (page 
-;    {:title "List Players"
-;     :body [:ol (map #([:li %1]) (nothing))]}))
-(def add-player-form (form-to [:post "/addPlayer.post"
-    [:fieldset [:legend "Add a new player"]
-        [:ol [:li [:label {:for :name} "Name"] (text-field :name)]
-             [:li [:label {:for :ranking} "Ranking"] (text-field :ranking)]]
-        [:button {:type "submit"} "Add!"]]]))
+(def add-player-form (form-to [:post "/addPlayer.post"]
+    [:fieldset 
+        [:legend "Add a new player"]
+            [:ol
+                [:li [:label {:for :email} "Email"] (text-field :email)]
+                [:li [:label {:for :name} "Name"] (text-field :name)]
+                [:li [:label {:for :ranking} "Ranking"] (text-field :ranking)]]
+        [:button {:type "submit"} "Add!"]]))
 
 (defn add-player [email name ranking]
-    ;(create-entity (player {:email email :name name :ranking ranking}))
+    (ds/create-entity {:kind "player" :email email :name name :ranking ranking})
     (redirect "/"))
 
+(defn get-players []
+    (ds/find-all (Query. "player")))
+(defn list-players-page [] (page 
+   {:title "List Players"
+    :body [:ol (map #(vector :li (:name %1)) (get-players))]}))
 (defroutes example
     (GET "/" [] (page {
         :title "This is the title"
         :body "<h1>Hello World Wide Web!</h1>"}))
-    (GET "/addPlayer" [] (page {:title "Add Player" :body (add-player-form)}))
-    (POST "/addPlayer.post" [name ranking] (add-player name ranking))
+    (GET "/addPlayer" [] (page {:title "Add Player" :body add-player-form}))
+    (POST "/addPlayer.post" [email name ranking] (add-player email name ranking))
+    (GET "/players" [] (list-players-page))
     ;(POST "/newSeason" [name] (create-season name))
     (route/not-found "Page not found"))
 
